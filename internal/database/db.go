cf5efaa8e1cc4b038c2962696f705d516544d580
IhsanAlhakim <ihsanalhakim04@gmail.com>
2026-04-17 14:53:06 +0700

    feat(database): add New() for create connection pool

---
+++ internal/database/db.go
@@ -0,0 +1,23 @@
+package database
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/IhsanAlhakim/socmed-backend-go/internal/config"
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+func New(dbConfig config.DBConfig) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dbConfig.Dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	log.Println("Connected to database")
+	return db, nil
+}
