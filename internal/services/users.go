698699ced43d386af8086f5d53d1b3a8f6b380bf
IhsanAlhakim <ihsanalhakim04@gmail.com>
2026-04-18 13:55:44 +0700

    feat(service): add DeleteUser to users service

--- internal/services/users.go
+++ internal/services/users.go
@@ -1,10 +1,13 @@
 package services
 
-import "github.com/IhsanAlhakim/socmed-backend-go/internal/store"
+import (
+	"github.com/IhsanAlhakim/socmed-backend-go/internal/store"
+)
 
 type UserServiceInterface interface {
 	CreateUser(userData store.User) error
 	UpdateUser(userData store.User) error
+	DeleteUser(userId int64)
 }
 
 type UserService struct {
@@ -32,6 +35,15 @@ func (usvc *UserService) CreateUser(userdata store.User) error {
 
 func (usvc *UserService) UpdateUser(userdata store.User) error {
 	err := usvc.storage.Users.Update(&userdata)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (usvc *UserService) DeleteUser(userId int64) error {
+
+	err := usvc.storage.Users.Delete(userId)
 	if err != nil {
 		return err
 	}
