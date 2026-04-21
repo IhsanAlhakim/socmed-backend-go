4edd3951ab0e5c1c0ccbfff72a3fbe2fb0d0462f
IhsanAlhakim <ihsanalhakim04@gmail.com>
2026-04-21 18:19:28 +0700

    refactor(follow): add omitempty on Follow struct attributes

--- internal/follows/follows_type.go
+++ internal/follows/follows_type.go
@@ -1,11 +1,6 @@
 package follows
 
-type Follow struct {
-	ID         int64 `json:"id"`
-	FollowedId int64 `json:"followed_id"`
-	FollowerId int64 `json:"follower_id"`
-}
-
+// Interface
 type StoreInterface interface {
 	Create(followData *FollowDataparam) error
 	Delete(followData *FollowDataparam) error
@@ -16,6 +11,13 @@ type ServiceInterface interface {
 	Follow(followData *FollowDataparam) error
 	Unfollow(followData *FollowDataparam) error
 	GetFollower(followedId int64) (*[]Follow, error)
+}
+
+// struct
+type Follow struct {
+	ID         int64 `json:"id,omitempty"`
+	FollowedId int64 `json:"followed_id,omitempty"`
+	FollowerId int64 `json:"follower_id,omitempty"`
 }
 
 type FollowDataparam struct {
