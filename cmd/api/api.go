6c04370dd7e1030d40986d40efe5ade38da2a6db
IhsanAlhakim <ihsanalhakim04@gmail.com>
2026-04-29 16:10:02 +0700

    feat(api): use chi router

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -12,6 +12,7 @@ import (
 	plikes "github.com/IhsanAlhakim/socmed-backend-go/internal/post_likes"
 	"github.com/IhsanAlhakim/socmed-backend-go/internal/posts"
 	"github.com/IhsanAlhakim/socmed-backend-go/internal/users"
+	"github.com/go-chi/chi/v5"
 )
 
 type application struct {
@@ -40,50 +41,50 @@ func newApp(db *sql.DB, config *config.Config) *application {
 }
 
 func (app *application) mount() http.Handler {
-	mux := http.NewServeMux()
+	r := chi.NewRouter()
 
-	mux.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("OK"))
 	})
 
 	userStore := users.NewStore(app.db)
 	userService := users.NewService(userStore, app.config)
 	userHandler := users.NewHandler(userService)
-	mux.HandleFunc("POST /users", userHandler.CreateUser)
-	mux.HandleFunc("PUT /users/{id}", userHandler.UpdateUser)
-	mux.HandleFunc("DELETE /users/{id}", userHandler.DeleteUser)
-	mux.HandleFunc("POST /sessions", userHandler.SignIn)
-	mux.HandleFunc("DELETE /sessions", userHandler.SignOut)
+	r.HandleFunc("POST /users", userHandler.CreateUser)
+	r.HandleFunc("PUT /users/{id}", userHandler.UpdateUser)
+	r.HandleFunc("DELETE /users/{id}", userHandler.DeleteUser)
+	r.HandleFunc("POST /sessions", userHandler.SignIn)
+	r.HandleFunc("DELETE /sessions", userHandler.SignOut)
 
 	postStore := posts.NewStore(app.db)
 	postService := posts.NewService(postStore)
 	postHandler := posts.NewHandler(postService)
-	mux.HandleFunc("GET /posts", postHandler.GetPosts)
-	mux.HandleFunc("GET /users/{userId}/followed-posts", postHandler.GetFollowedPosts)
-	mux.HandleFunc("GET /posts/{id}", postHandler.GetPostById)
-	mux.HandleFunc("POST /posts", postHandler.CreatePost)
-	mux.HandleFunc("DELETE /posts/{id}", postHandler.DeletePost)
+	r.HandleFunc("GET /posts", postHandler.GetPosts)
+	r.HandleFunc("GET /users/{userId}/followed-posts", postHandler.GetFollowedPosts)
+	r.HandleFunc("GET /posts/{id}", postHandler.GetPostById)
+	r.HandleFunc("POST /posts", postHandler.CreatePost)
+	r.HandleFunc("DELETE /posts/{id}", postHandler.DeletePost)
 
 	followStore := follows.NewStore(app.db)
 	followService := follows.NewService(followStore)
 	followHandler := follows.NewHandler(followService)
-	mux.HandleFunc("GET /users/{userId}/followers", followHandler.GetFollower)
-	mux.HandleFunc("GET /users/{userId}/following", followHandler.GetFollowed)
-	mux.HandleFunc("POST /users/{userId}/follow", followHandler.Follow)
-	mux.HandleFunc("DELETE /users/{userId}/follow", followHandler.Unfollow)
+	r.HandleFunc("GET /users/{userId}/followers", followHandler.GetFollower)
+	r.HandleFunc("GET /users/{userId}/following", followHandler.GetFollowed)
+	r.HandleFunc("POST /users/{userId}/follow", followHandler.Follow)
+	r.HandleFunc("DELETE /users/{userId}/follow", followHandler.Unfollow)
 
 	commentStore := comments.NewStore(app.db)
 	commentService := comments.NewService(commentStore)
 	commentHandler := comments.NewHandler(commentService)
-	mux.HandleFunc("POST /posts/{postId}/comments", commentHandler.CreateComment)
-	mux.HandleFunc("GET /posts/{postId}/comments", commentHandler.Getcomments)
-	mux.HandleFunc("DELETE /comments/{commentId}", commentHandler.DeleteComment)
+	r.HandleFunc("POST /posts/{postId}/comments", commentHandler.CreateComment)
+	r.HandleFunc("GET /posts/{postId}/comments", commentHandler.Getcomments)
+	r.HandleFunc("DELETE /comments/{commentId}", commentHandler.DeleteComment)
 
 	postLikesStore := plikes.NewStore(app.db)
 	postLikesService := plikes.NewService(postLikesStore)
 	postLikesHandler := plikes.NewHandler(postLikesService)
-	mux.HandleFunc("POST /posts/{postId}/likes", postLikesHandler.LikePost)
-	mux.HandleFunc("DELETE /posts/{postId}/likes", postLikesHandler.UnlikePost)
-	mux.HandleFunc("GET /posts/{postId}/likes", postLikesHandler.GetPostLiker)
-	return mux
+	r.HandleFunc("POST /posts/{postId}/likes", postLikesHandler.LikePost)
+	r.HandleFunc("DELETE /posts/{postId}/likes", postLikesHandler.UnlikePost)
+	r.HandleFunc("GET /posts/{postId}/likes", postLikesHandler.GetPostLiker)
+	return r
 }
