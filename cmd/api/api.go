5204e75869d3a3c58ceb7d184301725cab6613c1
IhsanAlhakim <ihsanalhakim04@gmail.com>
2026-05-04 15:27:17 +0700

    feat(api): use auth middleware on routes that require a logged in user

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -9,6 +9,7 @@ import (
 	"github.com/IhsanAlhakim/socmed-backend-go/internal/comments"
 	"github.com/IhsanAlhakim/socmed-backend-go/internal/config"
 	"github.com/IhsanAlhakim/socmed-backend-go/internal/follows"
+	"github.com/IhsanAlhakim/socmed-backend-go/internal/middlewares"
 	plikes "github.com/IhsanAlhakim/socmed-backend-go/internal/post_likes"
 	"github.com/IhsanAlhakim/socmed-backend-go/internal/posts"
 	"github.com/IhsanAlhakim/socmed-backend-go/internal/users"
@@ -30,6 +31,8 @@ func (app *application) run(mux http.Handler) error {
 		IdleTimeout:  time.Minute,
 	}
 
+	// add graceful shutdown
+
 	log.Println("Server has started at :8080")
 	return server.ListenAndServe()
 }
@@ -44,52 +47,66 @@ func newApp(db *sql.DB, config *config.Config) *application {
 func (app *application) mount() http.Handler {
 	r := chi.NewRouter()
 
+	m := middlewares.New(app.config)
+
 	r.Use(middleware.RequestID)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
+
+	userStore := users.NewStore(app.db)
+	userService := users.NewService(userStore, app.config)
+	userHandler := users.NewHandler(userService)
+
+	postStore := posts.NewStore(app.db)
+	postService := posts.NewService(postStore)
+	postHandler := posts.NewHandler(postService)
+
+	followStore := follows.NewStore(app.db)
+	followService := follows.NewService(followStore)
+	followHandler := follows.NewHandler(followService)
+
+	commentStore := comments.NewStore(app.db)
+	commentService := comments.NewService(commentStore)
+	commentHandler := comments.NewHandler(commentService)
+
+	postLikesStore := plikes.NewStore(app.db)
+	postLikesService := plikes.NewService(postLikesStore)
+	postLikesHandler := plikes.NewHandler(postLikesService)
 
 	r.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("OK"))
 	})
 
-	userStore := users.NewStore(app.db)
-	userService := users.NewService(userStore, app.config)
-	userHandler := users.NewHandler(userService)
 	r.HandleFunc("POST /users", userHandler.CreateUser)
-	r.HandleFunc("PUT /users/{id}", userHandler.UpdateUser)
-	r.HandleFunc("DELETE /users/{id}", userHandler.DeleteUser)
 	r.HandleFunc("POST /sessions", userHandler.SignIn)
-	r.HandleFunc("DELETE /sessions", userHandler.SignOut)
 
-	postStore := posts.NewStore(app.db)
-	postService := posts.NewService(postStore)
-	postHandler := posts.NewHandler(postService)
-	r.HandleFunc("GET /posts", postHandler.GetPosts)
-	r.HandleFunc("GET /users/{userId}/followed-posts", postHandler.GetFollowedPosts)
-	r.HandleFunc("GET /posts/{id}", postHandler.GetPostById)
-	r.HandleFunc("POST /posts", postHandler.CreatePost)
-	r.HandleFunc("DELETE /posts/{id}", postHandler.DeletePost)
+	// Endpoint with auth middleware
+	r.Route("/", func(r chi.Router) {
+		r.Use(m.Auth)
 
-	followStore := follows.NewStore(app.db)
-	followService := follows.NewService(followStore)
-	followHandler := follows.NewHandler(followService)
-	r.HandleFunc("GET /users/{userId}/followers", followHandler.GetFollower)
-	r.HandleFunc("GET /users/{userId}/following", followHandler.GetFollowed)
-	r.HandleFunc("POST /users/{userId}/follow", followHandler.Follow)
-	r.HandleFunc("DELETE /users/{userId}/follow", followHandler.Unfollow)
+		r.HandleFunc("PUT /users/{id}", userHandler.UpdateUser)
+		r.HandleFunc("DELETE /users/{id}", userHandler.DeleteUser)
+		r.HandleFunc("DELETE /sessions", userHandler.SignOut)
 
-	commentStore := comments.NewStore(app.db)
-	commentService := comments.NewService(commentStore)
-	commentHandler := comments.NewHandler(commentService)
-	r.HandleFunc("POST /posts/{postId}/comments", commentHandler.CreateComment)
-	r.HandleFunc("GET /posts/{postId}/comments", commentHandler.Getcomments)
-	r.HandleFunc("DELETE /comments/{commentId}", commentHandler.DeleteComment)
+		r.HandleFunc("GET /posts", postHandler.GetPosts)
+		r.HandleFunc("GET /users/{userId}/followed-posts", postHandler.GetFollowedPosts)
+		r.HandleFunc("GET /posts/{id}", postHandler.GetPostById)
+		r.HandleFunc("POST /posts", postHandler.CreatePost)
+		r.HandleFunc("DELETE /posts/{id}", postHandler.DeletePost)
 
-	postLikesStore := plikes.NewStore(app.db)
-	postLikesService := plikes.NewService(postLikesStore)
-	postLikesHandler := plikes.NewHandler(postLikesService)
-	r.HandleFunc("POST /posts/{postId}/likes", postLikesHandler.LikePost)
-	r.HandleFunc("DELETE /posts/{postId}/likes", postLikesHandler.UnlikePost)
-	r.HandleFunc("GET /posts/{postId}/likes", postLikesHandler.GetPostLiker)
+		r.HandleFunc("GET /users/{userId}/followers", followHandler.GetFollower)
+		r.HandleFunc("GET /users/{userId}/following", followHandler.GetFollowed)
+		r.HandleFunc("POST /users/{userId}/follow", followHandler.Follow)
+		r.HandleFunc("DELETE /users/{userId}/follow", followHandler.Unfollow)
+
+		r.HandleFunc("POST /posts/{postId}/comments", commentHandler.CreateComment)
+		r.HandleFunc("GET /posts/{postId}/comments", commentHandler.Getcomments)
+		r.HandleFunc("DELETE /comments/{commentId}", commentHandler.DeleteComment)
+
+		r.HandleFunc("POST /posts/{postId}/likes", postLikesHandler.LikePost)
+		r.HandleFunc("DELETE /posts/{postId}/likes", postLikesHandler.UnlikePost)
+		r.HandleFunc("GET /posts/{postId}/likes", postLikesHandler.GetPostLiker)
+	})
+
 	return r
 }
